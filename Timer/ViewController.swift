2e1671b82c9fde77865c42bd59121dce08197b1c
AngusYu <pighaddt@gmail.com>
2021-07-31 16:50:29 +0800

    timer setting play pause replay

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -8,10 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var time : Int = 0
+    var timer = Timer()
+    var isStart = false
+    @IBOutlet weak var labelTimer: UILabel!
+    @IBAction func pause(_ sender: Any) {
+        timer.invalidate()
+        isStart = false
+    }
+    @IBAction func play(_ sender: Any) {
+        if !isStart{
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+            timer.fire()
+            isStart = true
+        }else{
+            timer.invalidate()
+            isStart = false
+        }
+        
+    }
+    @IBAction func replay(_ sender: Any) {
+        timer.invalidate()
+        isStart = false
+        labelTimer.text = "0"
+        time = 0
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    
+    @objc func updateTimer(){
+        time = time + 1
+        labelTimer.text = String(time)
     }
 
 
